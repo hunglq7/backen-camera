184c845efd24ab2e746226441b9b6eaaf0ce0f4e
Le Quang Hung <hunglq7@gmail.com>
2025-12-28 18:10:36 +0700

    28.12.2025-18h10

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5001;
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // public routes
 app.use("/api/auth", authRoute);
